e7fbe1a1a4e830fe00b980b8a0cf893a0ea44127
James Holloway <james.holloway94@gmail.com>
2026-04-28 21:30:48 +0000

    inspect postgres lock wait sync for performance

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-04-28 20:12:15 UTC
+# Last sync: 2026-04-28 21:30:48 UTC
