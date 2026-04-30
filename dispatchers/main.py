098e12f243803d15a6289a1e03e2aade27878a80
James Holloway <james.holloway94@gmail.com>
2026-04-30 03:30:45 +0000

    chart redis memory fragmentation sync in metadata

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-04-30 03:03:18 UTC
+# Last sync: 2026-04-30 03:30:44 UTC
