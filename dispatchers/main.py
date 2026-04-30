d0da1373d53d0d25cc3f35a999be207f204d1250
James Holloway <james.holloway94@gmail.com>
2026-04-30 16:09:48 +0000

    recheck mongodb write concern sync after update

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-04-30 15:11:51 UTC
+# Last sync: 2026-04-30 16:09:48 UTC
