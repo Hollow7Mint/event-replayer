edb979fa1aa6492e78574cc6ba1e717b0160bcb2
James Holloway <james.holloway94@gmail.com>
2026-05-01 13:37:57 +0000

    lift ssh certificate expiry time into banner

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-01 12:17:52 UTC
+# Last sync: 2026-05-01 13:37:57 UTC
