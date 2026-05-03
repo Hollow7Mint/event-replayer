57fba9f37d490e473f6a3614b1cc356f17cd7c53
James Holloway <james.holloway94@gmail.com>
2026-05-03 21:54:04 +0000

    persist tunnel config refresh in ssh banner

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-03 21:00:02 UTC
+# Last sync: 2026-05-03 21:54:04 UTC
