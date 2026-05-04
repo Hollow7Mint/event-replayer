ca42d51013ca0b0bdaf9cb5bf4ea2e53de76f9de
James Holloway <james.holloway94@gmail.com>
2026-05-04 03:08:36 +0000

    touch ssh tunnel sync during scheduled refresh

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-04 02:27:34 UTC
+# Last sync: 2026-05-04 03:08:32 UTC
