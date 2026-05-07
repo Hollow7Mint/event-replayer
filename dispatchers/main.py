dbbc7828c4405364200107e89154d4cb9ba76863
James Holloway <james.holloway94@gmail.com>
2026-05-07 02:47:18 +0000

    stage postgres tablespace sync for scheduled refresh

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-07 01:46:38 UTC
+# Last sync: 2026-05-07 02:47:18 UTC
