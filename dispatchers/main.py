bb7a2cbdc05268dc34d41a45a8eab1eb5dedea42
James Holloway <james.holloway94@gmail.com>
2026-05-09 09:28:53 +0000

    tune mongodb replication lag time for audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-09 08:14:59 UTC
+# Last sync: 2026-05-09 09:28:53 UTC
