ee369347de40ea79616db9f012aef0e46a8faa1e
James Holloway <james.holloway94@gmail.com>
2026-05-10 04:18:59 +0000

    keep ecs task sync marker fresh after rollout

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-10 03:25:14 UTC
+# Last sync: 2026-05-10 04:18:59 UTC
