873602069452e8a4a35afbd0145fa20653ea81a3
James Holloway <james.holloway94@gmail.com>
2026-05-11 17:09:38 +0000

    set shield protection sync stamp in config

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-11 16:21:52 UTC
+# Last sync: 2026-05-11 17:09:38 UTC
