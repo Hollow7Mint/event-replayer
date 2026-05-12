d1f7a2a70aaacbbc60439ce87f042f1abb05c841
James Holloway <james.holloway94@gmail.com>
2026-05-12 15:30:25 +0000

    pin route53 dns sync stamp after record update

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-12 14:25:59 UTC
+# Last sync: 2026-05-12 15:30:25 UTC
