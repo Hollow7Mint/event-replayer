ca6c496f8628152c94bbce53968e56ec39ad10d3
James Holloway <james.holloway94@gmail.com>
2026-05-12 04:07:06 +0000

    note route53 health check sync in config record

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-12 02:32:40 UTC
+# Last sync: 2026-05-12 04:07:06 UTC
