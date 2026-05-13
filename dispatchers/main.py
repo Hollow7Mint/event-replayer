1b7246ee497a51bf50ba86dd789c0e270f5748f3
James Holloway <james.holloway94@gmail.com>
2026-05-13 21:20:22 +0000

    log shield ddos event time in protection banner

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-13 20:39:51 UTC
+# Last sync: 2026-05-13 21:20:22 UTC
