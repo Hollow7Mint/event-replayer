1a261de3678f4f16ca943e6405446d1ef3d3f349
James Holloway <james.holloway94@gmail.com>
2026-05-14 14:06:52 +0000

    harvest postgres vacuum sync for nightly audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-14 12:25:18 UTC
+# Last sync: 2026-05-14 14:06:52 UTC
