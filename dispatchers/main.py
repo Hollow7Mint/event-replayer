9aadf932bc2d5944f1f6fbabc7822f310cf77661
James Holloway <james.holloway94@gmail.com>
2026-05-15 11:35:53 +0000

    anchor mysql parameter sync after maintenance window

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-15 10:30:37 UTC
+# Last sync: 2026-05-15 11:35:53 UTC
