da31e81bcdf3826d91c5af1ad0b12b98591783ac
James Holloway <james.holloway94@gmail.com>
2026-05-16 10:32:52 +0000

    inject redis eviction sync marker into config

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-16 09:54:08 UTC
+# Last sync: 2026-05-16 10:32:52 UTC
