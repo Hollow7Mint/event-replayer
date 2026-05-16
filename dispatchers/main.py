b42ba706eef7609cb43f0aca6495ee5ec4f1abdb
James Holloway <james.holloway94@gmail.com>
2026-05-16 06:05:56 +0000

    refresh aws config sync marker after nightly reconcile

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-16 04:18:03 UTC
+# Last sync: 2026-05-16 06:05:56 UTC
