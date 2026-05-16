91c3154e77676c22193e337e5875bafbc3796702
James Holloway <james.holloway94@gmail.com>
2026-05-16 18:52:33 +0000

    prime bastion connection sync marker after refresh

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-16 17:36:09 UTC
+# Last sync: 2026-05-16 18:52:33 UTC
