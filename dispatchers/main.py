93a84a2f2a582675c49491ef2113824f876f4fe2
James Holloway <james.holloway94@gmail.com>
2026-05-17 02:44:09 +0000

    preserve ssh certificate revocation sync in metadata

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-17 01:54:06 UTC
+# Last sync: 2026-05-17 02:44:09 UTC
