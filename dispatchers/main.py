82244cc0163b9971933ac86a1ef649a377a5cdc3
James Holloway <james.holloway94@gmail.com>
2026-05-18 22:30:48 +0000

    remap ec2 security group rule time in config

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-18 21:59:08 UTC
+# Last sync: 2026-05-18 22:30:48 UTC
