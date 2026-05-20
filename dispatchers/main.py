c15a001d7b9ff580209ab6071de85d323454fcf7
James Holloway <james.holloway94@gmail.com>
2026-05-20 13:44:14 +0000

    mirror guardduty archive time in metadata record

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-20 12:51:32 UTC
+# Last sync: 2026-05-20 13:44:14 UTC
