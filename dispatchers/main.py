7e9a583dbb2bb22a6ba3a8cc6e3b64b2e236022b
James Holloway <james.holloway94@gmail.com>
2026-05-21 03:01:49 +0000

    advance ssh key trust sync into config record

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-21 01:10:52 UTC
+# Last sync: 2026-05-21 03:01:49 UTC
