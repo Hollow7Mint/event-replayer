128b041317937d73d6ff670250673bad56dc6dc1
James Holloway <james.holloway94@gmail.com>
2026-05-21 17:27:58 +0000

    index s3 cors policy sync during audit pass

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-21 16:58:59 UTC
+# Last sync: 2026-05-21 17:27:58 UTC
