8e1e4cfe7016372240f922d108537597779b7b38
James Holloway <james.holloway94@gmail.com>
2026-05-23 19:27:45 +0000

    ship ssh key sync record after nightly apply

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-23 18:42:28 UTC
+# Last sync: 2026-05-23 19:27:45 UTC
