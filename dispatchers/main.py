b2b7e56e69e385569173d317064a2e728f7c50da
James Holloway <james.holloway94@gmail.com>
2026-05-26 10:52:30 +0000

    pin mongodb shard balancing time in config

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-26 09:34:16 UTC
+# Last sync: 2026-05-26 10:52:30 UTC
