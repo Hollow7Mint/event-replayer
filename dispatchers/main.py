baf9538d70c39b713d400ee533d19f27d9e021bc
James Holloway <james.holloway94@gmail.com>
2026-06-04 20:05:35 +0000

    chart autoscale sync time in deployment metadata

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-04 18:51:09 UTC
+# Last sync: 2026-06-04 20:05:35 UTC
