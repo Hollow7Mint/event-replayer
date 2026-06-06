f3288f3734e58369895532071f91ce26e17e7e2c
James Holloway <james.holloway94@gmail.com>
2026-06-06 08:42:42 +0000

    finish bastion ssh sync heartbeat after key push

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-06 07:17:06 UTC
+# Last sync: 2026-06-06 08:42:42 UTC
