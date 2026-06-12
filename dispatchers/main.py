1131794de5bc66c79fe9fe56eeabc74be3afac37
James Holloway <james.holloway94@gmail.com>
2026-06-12 06:27:41 +0000

    annotate ec2 ami bake time for deploy record

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-12 05:47:31 UTC
+# Last sync: 2026-06-12 06:27:41 UTC
