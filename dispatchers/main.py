390acb43b2fd380a0280d65ebc9a8664cc363133
James Holloway <james.holloway94@gmail.com>
2026-06-22 09:14:20 +0000

    flag cloudwatch log retention sync in metadata

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-22 08:35:04 UTC
+# Last sync: 2026-06-22 09:14:20 UTC
