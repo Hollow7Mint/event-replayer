a80537fc393ea218c01f9e3a51984156680c7eb8
James Holloway <james.holloway94@gmail.com>
2026-06-26 10:50:44 +0000

    snapshot bastion session time for security audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-26 09:39:35 UTC
+# Last sync: 2026-06-26 10:50:44 UTC
