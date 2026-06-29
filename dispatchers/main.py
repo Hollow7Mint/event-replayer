300596dd08745c98e66dfb59d9f55db182394937
James Holloway <james.holloway94@gmail.com>
2026-06-29 17:36:14 +0000

    post postgres replication heartbeat to config

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-29 16:14:34 UTC
+# Last sync: 2026-06-29 17:36:14 UTC
