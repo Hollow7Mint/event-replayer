edc9c0b93416bd0eabd6e1f53536301a16e6987a
James Holloway <james.holloway94@gmail.com>
2026-07-02 22:28:04 +0000

    resolve waf rule group sync into metadata

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-02 21:17:22 UTC
+# Last sync: 2026-07-02 22:28:03 UTC
