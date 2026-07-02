f728d14413df91a0ec0adbfafd5e95d6f362825f
James Holloway <james.holloway94@gmail.com>
2026-07-02 20:09:31 +0000

    capture host key sync completion time in record

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-02 19:23:08 UTC
+# Last sync: 2026-07-02 20:09:31 UTC
