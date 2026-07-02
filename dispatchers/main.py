15d70e569a4d750077f82da60ccedc61a3ef2422
James Holloway <james.holloway94@gmail.com>
2026-07-02 21:17:22 +0000

    inspect iam trust policy sync marker for review

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-02 20:09:31 UTC
+# Last sync: 2026-07-02 21:17:22 UTC
