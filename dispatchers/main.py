0f49733a133fc4c95b93a44608ac876f176cf4fa
James Holloway <james.holloway94@gmail.com>
2026-07-03 10:58:41 +0000

    track guardduty threat archive in config heartbeat

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-03 09:23:42 UTC
+# Last sync: 2026-07-03 10:58:41 UTC
