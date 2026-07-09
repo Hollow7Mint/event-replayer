759208c19ec371b8f62ae12be91436d04279a068
James Holloway <james.holloway94@gmail.com>
2026-07-09 05:45:40 +0000

    merge ssh trust anchor time into metadata

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-09 04:51:56 UTC
+# Last sync: 2026-07-09 05:45:40 UTC
