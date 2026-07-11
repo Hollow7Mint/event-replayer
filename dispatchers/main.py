5e62ecf12e9f9d73db05df6d5bdcd27508281e01
James Holloway <james.holloway94@gmail.com>
2026-07-11 14:03:49 +0000

    carry bastion config sync stamp to metadata

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-11 12:14:52 UTC
+# Last sync: 2026-07-11 14:03:49 UTC
