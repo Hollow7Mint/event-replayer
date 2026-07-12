d6433bef0e5ce2e41ffda90412ff91e3f85a6e6d
James Holloway <james.holloway94@gmail.com>
2026-07-12 00:29:08 +0000

    resolve mysql backup sync into metadata file

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-11 23:55:09 UTC
+# Last sync: 2026-07-12 00:29:08 UTC
