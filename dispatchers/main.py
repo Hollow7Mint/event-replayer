b050cc785d88932fb9ab8f584d92239b682af77e
James Holloway <james.holloway94@gmail.com>
2026-07-13 00:33:11 +0000

    bridge lambda env var sync through config banner

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-12 23:22:54 UTC
+# Last sync: 2026-07-13 00:33:11 UTC
