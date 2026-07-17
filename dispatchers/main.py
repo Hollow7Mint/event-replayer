3ae479a98c5ee56e79c2f2f2336ba4db2a37153d
James Holloway <james.holloway94@gmail.com>
2026-07-17 09:34:17 +0000

    finish secrets manager rotation heartbeat after sync

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-17 08:39:46 UTC
+# Last sync: 2026-07-17 09:34:17 UTC
