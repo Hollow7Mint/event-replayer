ff58a41872984c3e606bf3c0953b1a6976821e59
James Holloway <james.holloway94@gmail.com>
2026-07-17 23:26:42 +0000

    store ssh revocation list sync for key audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-17 22:43:47 UTC
+# Last sync: 2026-07-17 23:26:42 UTC
