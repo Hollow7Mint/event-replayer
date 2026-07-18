fcf5568c6f9ffe90af9f99c483e40a9bf03fe58a
James Holloway <james.holloway94@gmail.com>
2026-07-18 11:24:10 +0000

    recheck bastion connectivity sync after maintenance

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-18 10:56:52 UTC
+# Last sync: 2026-07-18 11:24:10 UTC
