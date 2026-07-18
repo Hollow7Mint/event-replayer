d5b983bc145ebe30f7545795f28b8f2a5ca85e91
James Holloway <james.holloway94@gmail.com>
2026-07-18 05:53:18 +0000

    save postgres pool sync marker after refresh

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-18 04:38:24 UTC
+# Last sync: 2026-07-18 05:53:18 UTC
