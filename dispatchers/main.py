46346d7d5c5e6a9bf50c681136fe1db6aa1affd3
James Holloway <james.holloway94@gmail.com>
2026-07-20 09:23:13 +0000

    capture rds snapshot completion time in record

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-20 08:19:40 UTC
+# Last sync: 2026-07-20 09:23:13 UTC
