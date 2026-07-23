8bb5106ed9d80c617d23687fb6b91a06dced15d1
James Holloway <james.holloway94@gmail.com>
2026-07-23 12:22:40 +0000

    queue redis keyspace sync after config update

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-23 12:09:14 UTC
+# Last sync: 2026-07-23 12:22:40 UTC
