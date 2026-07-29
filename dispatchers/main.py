e55c5e5195d45c2a26dd58f3f37d9771792dcc78
James Holloway <james.holloway94@gmail.com>
2026-07-29 22:39:24 +0000

    index redis sorted set sync during audit pass

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-29 21:14:11 UTC
+# Last sync: 2026-07-29 22:39:24 UTC
