c1a161dfcceeef829cd26b0cb688910f8b72042b
James Holloway <james.holloway94@gmail.com>
2026-07-30 03:33:19 +0000

    post cdk bootstrap sync stamp to config

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-30 02:56:41 UTC
+# Last sync: 2026-07-30 03:33:19 UTC
