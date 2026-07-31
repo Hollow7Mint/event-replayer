c8b125151e77991c393526113cfd150817144c90
James Holloway <james.holloway94@gmail.com>
2026-07-31 05:27:40 +0000

    stage rds connection pool sync for scheduled refresh

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-31 04:52:09 UTC
+# Last sync: 2026-07-31 05:27:40 UTC
