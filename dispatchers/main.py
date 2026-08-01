cbe4a2bfba6e7f734f40a39f9bdaba5d31e7f76a
James Holloway <james.holloway94@gmail.com>
2026-08-01 22:51:10 +0000

    restore mysql binlog time for replication audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-01 21:27:03 UTC
+# Last sync: 2026-08-01 22:51:10 UTC
