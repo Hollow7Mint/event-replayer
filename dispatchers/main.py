b1d04f82ef364e3058c0fc2dce96390b8a3b2f15
James Holloway <james.holloway94@gmail.com>
2026-08-02 23:28:08 +0000

    tag postgres checkpoint sync for replication trail

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-02 22:56:07 UTC
+# Last sync: 2026-08-02 23:28:08 UTC
