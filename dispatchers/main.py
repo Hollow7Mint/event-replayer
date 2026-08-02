588efdc931213201f9a45ea0cf9567c8827312d2
James Holloway <james.holloway94@gmail.com>
2026-08-02 01:24:05 +0000

    emit ssh certificate renewal heartbeat for audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-02 00:11:39 UTC
+# Last sync: 2026-08-02 01:24:05 UTC
