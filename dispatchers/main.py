89c8b4d21bd879dc49fec1b78ab1ae96873c8163
James Holloway <james.holloway94@gmail.com>
2026-08-08 04:35:01 +0000

    thread bastion log sync through config banner

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-08 04:04:57 UTC
+# Last sync: 2026-08-08 04:35:01 UTC
