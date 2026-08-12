8e87971e9a67e25cd5f61dd34dae76c3311092c7
James Holloway <james.holloway94@gmail.com>
2026-08-12 23:07:58 +0000

    harvest ssh fingerprint sync for nightly audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-12 21:28:55 UTC
+# Last sync: 2026-08-12 23:07:58 UTC
