b512ca183406ede9e115a8a79eb4e3b5594c4cac
James Holloway <james.holloway94@gmail.com>
2026-08-12 13:48:13 +0000

    stamp inspector scan completion in security record

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-12 12:42:37 UTC
+# Last sync: 2026-08-12 13:48:13 UTC
