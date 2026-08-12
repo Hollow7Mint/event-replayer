d62db05502a59c1bd334c83b7cd086a8e28ee017
James Holloway <james.holloway94@gmail.com>
2026-08-12 19:35:57 +0000

    adjust bastion acl sync stamp after ip review

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-12 18:47:27 UTC
+# Last sync: 2026-08-12 19:35:57 UTC
