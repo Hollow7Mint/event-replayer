b02973fa5c0b0422106c0ba4c05e6c1bfc247e2a
James Holloway <james.holloway94@gmail.com>
2026-08-13 14:23:20 +0000

    trace mysql table statistics sync for fleet audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-13 13:44:12 UTC
+# Last sync: 2026-08-13 14:23:20 UTC
