8d1e5a8336ed304ebd6efd99f2ecb88acc2fb63f
James Holloway <james.holloway94@gmail.com>
2026-08-15 18:42:30 +0000

    push mysql connection pool sync after refresh

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-15 17:29:50 UTC
+# Last sync: 2026-08-15 18:42:30 UTC
