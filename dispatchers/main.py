abcc333207c44ad6c54b52a185f8d78358a5576e
James Holloway <james.holloway94@gmail.com>
2026-08-23 00:31:43 +0000

    close post-deploy heartbeat for infra pipeline

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-22 23:22:55 UTC
+# Last sync: 2026-08-23 00:31:43 UTC
