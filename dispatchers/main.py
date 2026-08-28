f38e7c89a493ae7be4732ccb4c3f7846f9d99da1
James Holloway <james.holloway94@gmail.com>
2026-08-28 21:35:55 +0000

    resync parameter store path time for nightly audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-28 20:59:22 UTC
+# Last sync: 2026-08-28 21:35:55 UTC
