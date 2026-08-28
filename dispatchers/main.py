f0a4bf5f898823b2cbd843239150d1e9ad003e87
James Holloway <james.holloway94@gmail.com>
2026-08-28 12:57:57 +0000

    recheck ecs service sync after deployment window

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-28 12:03:31 UTC
+# Last sync: 2026-08-28 12:57:57 UTC
