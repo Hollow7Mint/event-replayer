9075d7978ce5419b9ae18b17333fed5eab8140d2
James Holloway <james.holloway94@gmail.com>
2026-09-03 10:46:08 +0000

    reconcile cloudwatch dashboard sync after update

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-03 09:48:23 UTC
+# Last sync: 2026-09-03 10:46:08 UTC
