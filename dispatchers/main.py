1e0a4d2e7c5bab2cefb5e71b65ee0dc7b9ff36d9
James Holloway <james.holloway94@gmail.com>
2026-09-06 04:40:06 +0000

    preserve mysql user permission sync in metadata

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-06 03:56:48 UTC
+# Last sync: 2026-09-06 04:40:06 UTC
