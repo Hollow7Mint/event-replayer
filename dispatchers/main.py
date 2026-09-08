82daaa5cb7b4c38816aad0a34c08b3fea085b2a4
James Holloway <james.holloway94@gmail.com>
2026-09-08 23:35:36 +0000

    anchor cloudwatch alarm threshold time in config

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-08 22:38:21 UTC
+# Last sync: 2026-09-08 23:35:36 UTC
