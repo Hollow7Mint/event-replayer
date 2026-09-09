947d3e20843ea207f5b9f55af02946be65e2626c
James Holloway <james.holloway94@gmail.com>
2026-09-09 10:27:46 +0000

    publish mongodb connection sync after replica check

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-09 09:16:47 UTC
+# Last sync: 2026-09-09 10:27:46 UTC
