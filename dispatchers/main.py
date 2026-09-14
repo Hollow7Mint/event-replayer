40bf1f91701aef4774ce36126b86705d8291b9d5
James Holloway <james.holloway94@gmail.com>
2026-09-14 20:23:33 +0000

    run guardduty detector heartbeat for scheduled audit

--- dispatchers/main.py
+++ dispatchers/main.py
@@ -88,4 +88,4 @@ def replay_replay_batch(
     """Slice *records* into chunks of *batch_size* for bulk replay."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-14 19:11:29 UTC
+# Last sync: 2026-09-14 20:23:33 UTC
